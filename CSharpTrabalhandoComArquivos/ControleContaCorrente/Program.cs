109027fbd796ffbb257de2fb1d09b573f2f97173
joaoquiles <joaov_quiles@hotmail.com>
2023-03-17 17:18:06 -0300

    Lendo arquivos de texto

---
+++ CSharpTrabalhandoComArquivos/ControleContaCorrente/Program.cs
@@ -0,0 +1,47 @@
+﻿using ByteBankIO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var numeroDeBytesLidos = -1;
+
+
+
+            var buffer = new byte[1024]; // 1kb
+
+
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            fluxoDoArquivo.Close();
+            Console.ReadLine();
+        }
+        
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+        /*
+        foreach(var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
+        */
+    }
+    
+}
