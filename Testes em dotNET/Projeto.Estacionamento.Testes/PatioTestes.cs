92c36919fc379228beee37fb4e63a62efcf5b5d9
joaoquiles <joaov_quiles@hotmail.com>
2023-04-24 16:29:07 -0300

    Testes com xUnit

---
+++ Testes em dotNET/Projeto.Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,35 @@
+﻿using Projeto.Estacionamento.Modelos;
+using Projeto.Estacionamento.Projeto.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = "Joao";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Azul";
+            veiculo.Modelo = "Mustang";
+            veiculo.Placa = "asd-9999";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2,faturamento);
+        }
+    }
+}
---
+++ Testes em dotNET/Projeto.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,29 @@
+using Projeto.Estacionamento.Modelos;
+
+namespace Projeto.Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Acelerar(10);
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Frear(10);
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
